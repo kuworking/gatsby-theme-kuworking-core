80ed637e618b899929ded5925786fdcd99731848
kuworking <kuworking@gmail.com>
2020-04-07 20:11:09 +0200

    .

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
-export { GoogleMap } from './src/components/providers/googlemap'
-export { Instagram } from './src/components/providers/instagram'
-export { Pinterest, PinterestScript } from './src/components/providers/pinterest'
-export { Youtube } from './src/components/providers/youtube'
-export { Video } from './src/components/providers/video'
-
-export { Spinner01 } from './src/components/elements/spinner'
-export { Switch } from './src/components/elements/switch'
-
-export { Disqus } from './src/components/disqus/disqus'
+export { Pagination } from './src/components/pagination'
+export { config, text, seoText } from './src/utils/config'
+export { Page } from './src/templates/page-template'
 
 export {
-  shuffle_array,
+  SEO,
+  Margin,
+  q,
+  qq,
+  Switch,
+  Spinner01,
+  SocialShare,
+  Box,
+  Fade,
+  Toggle,
+  Move,
+  Neutral,
+  TextLetterFade,
+  TextLetterFade2,
+  TextPeek,
+  TextShadow,
+  TextShow,
+  TextZoom,
+  TextZoomPanic,
+  domprogress,
+  useCss,
+  useFlexMasonry,
+  useGridMasonry,
+  useLocalStorage,
+  useScript,
+  useWindowResize,
+  useInterval,
+  Disqus,
+  CodeLink,
+  useCodePenScript,
+  CodePen,
+  Highlight,
+  apply_highlight,
+  BImg,
+  Img,
   fixDate,
+  shuffle_array,
   is_this_a_mobile_device,
   wait,
   get_response,
   get_request,
-} from './src/components/methods/methods'
-export { BImg, Img } from './src/components/methods/image'
-export { Highlight, apply_highlight } from './src/components/methods/highlight'
-export { Prism } from './src/components/methods/prism'
-export { CodeLink, useCodePenScript, CodePen } from './src/components/methods/codepen'
-
-export { useScript } from './src/components/hooks/usescript'
-export { useCss } from './src/components/hooks/usecss'
-export { useGridMasonry } from './src/components/hooks/usegridmasonry'
-export { useFlexMasonry } from './src/components/hooks/useflexmasonry'
-export { progress } from './src/components/hooks/domprogress'
-export { useLocalStorage } from './src/components/hooks/uselocalstorage'
-export { useWindowResize } from './src/components/hooks/usewindowresize'
-
-
-export { Pagination } from './src/components/pagination'
-export { SEO } from './src/components/seo'
-export { SocialShare } from './src/components/socialshare'
-export { Config, Text, SeoText } from './src/utils/config'
-export { Page } from './src/templates/page-template'
+  Prism,
+  GoogleMap,
+  Instagram,
+  Pinterest,
+  PinterestScript,
+  Video,
+  Youtube,
+} from 'gatsby-theme-kuworking-methods'
